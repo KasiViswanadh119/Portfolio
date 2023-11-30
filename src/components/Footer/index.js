53d1900f5473f1da3485d58a9a70f1690607b8ec
Kasi Viswanadh Duvva <21mh5a4916@acoe.edu.in>
2023-11-30 16:49:40 +0530

    commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -88,7 +88,7 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Logo>Rishav Chanda</Logo>
+        <Logo>Kasi Viswanadh</Logo>
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
