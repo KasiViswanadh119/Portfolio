ef4cfc6d0eb6d0f39a819c7b8935fb66a73a1f70
Kasi Viswanadh Duvva <21mh5a4916@acoe.edu.in>
2024-09-02 09:51:15 +0530

    resumeupdates

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/KasiViswanadh119",
   resume:
-    "https://drive.google.com/file/d/1hSXBvCP4GtsmroTelAHZ-mbx3lTnOG3z/view?usp=drive_link",
+    "https://drive.google.com/file/d/1tqgunQ5xipSFv1I7mBHdGBqReoE19r-K/view",
   linkedin: "https://www.linkedin.com/in/kasi-viswanadh-3b5740246",
   twitter: "",
   insta: "",
